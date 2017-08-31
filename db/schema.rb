33a1f60710828932cac89f87dfb7d38b79bc6bf5
maxantonio <mycybornet@gmail.com>
2017-08-31 10:15:17 -0400

    tmp

--- db/schema.rb
+++ db/schema.rb
@@ -68,10 +68,10 @@ ActiveRecord::Schema.define(version: 20170830175853) do
     t.integer "permission_level", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.string "username"
     t.string "first_name"
     t.string "last_name"
-    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
